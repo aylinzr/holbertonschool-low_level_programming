418473eaa25ec09d5c588f9881d514741ed55ccc
aylinzr <aylinozerpro@gmail.com>
2025-02-17 17:20:07 +0000

    5printfc

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Description: Affiche une phrase avec une grammaire correcte.
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
