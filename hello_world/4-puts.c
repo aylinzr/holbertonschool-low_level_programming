cd1429f5bf0df9dbd12e9d3d3555f16c6d59a334
aylinzr <aylinozerpro@gmail.com>
2025-02-17 14:48:55 +0000

    4putsc

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Point d'entrée du programme
+ *
+ * Return: Toujours 0 (Succès)
+ */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
