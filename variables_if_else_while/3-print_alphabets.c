6bf2132463a0fa4a596a0ebe0a8c1f69aa3b989f
aylinzr <aylinozerpro@gmail.com>
2025-02-19 09:40:47 +0000

    3printalphabetsc

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - Point d'entrée du programme
- * Description: Affiche l'alphabet en minuscules et majuscules, suivi d'une nouvelle ligne
+ * Description: Affiche l'alphabet en minuscules et majuscules,
+ * suivi d'une nouvelle ligne
+ * Return: Retourne 0 pour indiquer que le programme s'est exécuté correctent
  */
+
 int main(void)
 {
 	char c;
 
 	c = 'a';
-	while(c <= 'z')
+	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
-	
 	c = 'A';
-	while(c <= 'Z')
+	while (c <= 'Z')
 	{
 		putchar(c);
 		c++;
