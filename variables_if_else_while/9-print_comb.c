4c00fdbf042bd0f91320cd117ebdea5fddb442cc
aylinzr <aylinozerpro@gmail.com>
2025-02-19 23:05:22 +0000

    9printcomb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,9 +10,9 @@ int main(void)
 	int n;
 
 	for (n = 48; n < 58; n++)  /* 48 = '0' et 58 = '9' + 1 en ASCII */
-	{
 	putchar(n);
-	if (n != 57)  /* Ne pas ajouter de virgule après le dernier chiffre */
+	if (n != 57)
+	{
 		{
 			putchar(',');
 			putchar(' ');
