e2d1771cc69329f98b2ada0e3409eceb255b4a64
aylinzr <aylinozerpro@gmail.com>
2025-02-21 09:29:19 +0000

    2printalphabetx10c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Affiche l'alphabet en minuscules suivi d'une nouvelle ligne
- * Description: Cette fonction utilise `_putchar` pour afficher
- * chaque lettre de l'alphabet en minuscule, puis une nouvelle ligne.
+ * print_alphabet_x10 - Affiche l'alphabet en minuscules 10 fois
+ * Description: Cette fonction utilise deux boucles imbriquées.
+ * La première boucle exécute 10 fois l'affichage de l'alphabet.
+ * La seconde boucle affiche chaque lettre de 'a' à 'z'.
+ * Chaque alphabet affiché est suivi d'une nouvelle ligne.
  */
 
 void print_alphabet_x10(void)
@@ -11,7 +13,7 @@ void print_alphabet_x10(void)
 	char c;
 	int i;
 
-	for (i = '1'; i <= 10; i++)
+	for (i = '1'; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 
