b3af8d3c3f30f06b18d85120f981e295804787ca
aylinzr <aylinozerpro@gmail.com>
2025-02-25 14:02:57 +0000

    100primefactor

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Trouve et affiche le plus grand facteur
+ *        premier du nombre 612852475143.
+ * Return: Toujours 0.
+ */
+
+int main(void)
+{
+	long prime = 612852475143, div;
+
+	while (div < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+
+		for (div = 3; div < (prime / 2); div +=2)
+		{
+			if ((prime % div) == 0)
+				prime /= div;
+		}
+	}
+	
+	printf("%ld\n", prime);
+
+	return (0);
+}
